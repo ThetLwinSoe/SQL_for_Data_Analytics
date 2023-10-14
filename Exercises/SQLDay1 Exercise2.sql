e997c7d5e30305e552542ee0c7f63259b2fa0934
ThetLwinSoe <thetlwinsoe@gmail.com>
2023-10-14 13:14:17 +0630

    SQL Day1

---
+++ Exercises/SQLDay1 Exercise2.sql
@@ -0,0 +1,44 @@
+﻿use AdventureWorks2022
+
+SELECT *
+FROM [Person].[Person]
+
+
+SELECT [FirstName]
+FROM [Person].[Person]
+
+
+SELECT [LastName]
+FROM [Person].[Person]
+
+
+SELECT [FirstName],[LastName]
+FROM [Person].[Person]
+
+
+SELECT [FirstName],[MiddleName],[LastName]
+FROM [Person].[Person]
+
+
+SELECT Top 500 [FirstName],[MiddleName],[LastName]
+FROM [Person].[Person]
+
+
+SELECT Top 10 Percent [FirstName],[MiddleName],[LastName]
+FROM [Person].[Person]
+
+
+SELECT Top 100 *
+FROM [Person].[Person]
+
+
+SELECT Top 100
+	[FirstName] AS "Customer First Name",
+	[LastName] AS "Customer Last Name"
+FROM [Person].[Person]
+
+
+SELECT Top 100
+	[FirstName] "Customer First Name",
+	[LastName] "Customer Last Name"
+FROM [Person].[Person]
--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'Pa Pa'
---SELECT ‘Pa’, ‘Pa’
---SELECT ‘Pa’, ‘Pa’, ‘Databuzz KP’
---SELECT 1+1
---SELECT 6-2
---SELECT 4*4
---SELECT 4/4
---SELECT (5*5) – (3*5)
